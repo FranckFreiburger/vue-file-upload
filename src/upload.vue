f9721a53885499671f61ef32b8c76e2c28208f59
Franck Freiburger <franck.freiburger@gmail.com>
2018-01-09 15:39:56 +0100

    - allow ssr (still need vue-no-ssr to work properly)

--- src/upload.vue
+++ src/upload.vue
@@ -140,308 +140,315 @@
 <script>
 "use strict";
 
-var hasFileAPI = window.FileReader && window.FormData;
+if ( typeof(process) === 'undefined' || process.env.VUE_ENV === 'client' ) {
 
-// hasFileAPI = false
+	var hasFileAPI = window.FileReader && window.FormData;
 
-function isIFrameWindow(iframeElt) {
-	
-	try {
-		return iframeElt.contentWindow;
-	} catch(ex) {}
-	return null;
-}
+	// hasFileAPI = false
 
-function hasDataTransferFileSupport(dataTransfer) {
-	
-	if ( !('types' in dataTransfer) )
+	function isIFrameWindow(iframeElt) {
+		
+		try {
+			return iframeElt.contentWindow;
+		} catch(ex) {}
+		return null;
+	}
+
+	function hasDataTransferFileSupport(dataTransfer) {
+		
+		if ( !('types' in dataTransfer) )
+			return false;
+		for ( var i = 0; i < dataTransfer.types.length; ++i )
+			if ( dataTransfer.types[i] === 'Files' )
+				return true;
 		return false;
-	for ( var i = 0; i < dataTransfer.types.length; ++i )
-		if ( dataTransfer.types[i] === 'Files' )
-			return true;
-	return false;
-}
+	}
 
-module.exports = {
-	props: {
-		url: {
-			type: String,
-			required: true,
+	module.exports = {
+		props: {
+			url: {
+				type: String,
+				required: true,
+			},
+			multiple: {
+				type: Boolean,
+				default: false,
+			},
+			image: {
+				type: Boolean,
+				default: false,
+			},
+			capture: {
+				type: Boolean,
+				default: false,
+			},
+			accept: {
+				type: Function,
+				default: function(filename) { return true },
+			},
+			done: {
+				type: Function,
+				default: function(status, responseText, feedback) { status !== undefined && feedback(status >= 200 && status < 400) },
+			},
+			data: {
+				type: Object,
+			}
 		},
-		multiple: {
-			type: Boolean,
-			default: false,
-		},
-		image: {
-			type: Boolean,
-			default: false,
-		},
-		capture: {
-			type: Boolean,
-			default: false,
-		},
-		accept: {
-			type: Function,
-			default: function(filename) { return true },
-		},
-		done: {
-			type: Function,
-			default: function(status, responseText, feedback) { status !== undefined && feedback(status >= 200 && status < 400) },
-		},
-		data: {
-			type: Object,
-		}
-	},
-	data: function() {
-		return {
-			hasFileAPI: hasFileAPI,
-			dropState: '',
-			uploadState: '',
-			uploads: [],
-			total: 0,
-			loaded: 0,
-			tick: 0,
-		}
-	},
-	
-	computed: {
-		uploadInfo: function() {
-			
-			var filenameList = [];
-			for ( var i = 0; i < this.uploads.length; ++i )
-				Array.prototype.push.apply(filenameList, this.uploads[i].filenameList);
-			return filenameList.map(function(item) { return '\u2022 ' + item }).join('\n');
-		},
-		progressStyle: function() {
-			
-			if ( this.uploads.length === 0 )
-				return undefined;
-			
-			var loadedRatio = this.loaded / this.total;
-			if ( isNaN(loadedRatio) )
-				return { width: '50%', marginLeft: (50 - Math.abs(this.tickNext() % 50*2 - 50))+'%' };
-			else
-				return { width: loadedRatio*100+'%' };
-		},
-	},
-	
-	watch: {
-		'uploads.length': function(length) {
-			
-			if ( length !== 0 )
-				return;
-			this.loaded = 0;
-			this.total = 0;
-		}
-	},
-	
-	methods: {
-		tickNext: function() {
-
-			window.setTimeout(function() {
-			
-				this.tick++;
-			}.bind(this), 250);
-			return this.tick;
+		data: function() {
+			return {
+				hasFileAPI: hasFileAPI,
+				dropState: '',
+				uploadState: '',
+				uploads: [],
+				total: 0,
+				loaded: 0,
+				tick: 0,
+			}
 		},
 		
-		uploaded: function(status, responseText) {
-			
-			var feedback = function(success) {
-			
-				this.uploadState = success ? 'uploadSuccess' : 'uploadFailure';
+		computed: {
+			uploadInfo: function() {
+				
+				var filenameList = [];
+				for ( var i = 0; i < this.uploads.length; ++i )
+					Array.prototype.push.apply(filenameList, this.uploads[i].filenameList);
+				return filenameList.map(function(item) { return '\u2022 ' + item }).join('\n');
+			},
+			progressStyle: function() {
+				
+				if ( this.uploads.length === 0 )
+					return undefined;
+				
+				var loadedRatio = this.loaded / this.total;
+				if ( isNaN(loadedRatio) )
+					return { width: '50%', marginLeft: (50 - Math.abs(this.tickNext() % 50*2 - 50))+'%' };
+				else
+					return { width: loadedRatio*100+'%' };
+			},
+		},
+		
+		watch: {
+			'uploads.length': function(length) {
+				
+				if ( length !== 0 )
+					return;
+				this.loaded = 0;
+				this.total = 0;
+			}
+		},
+		
+		methods: {
+			tickNext: function() {
+
 				window.setTimeout(function() {
 				
-					this.uploadState = '';
-				}.bind(this), 1000);
-			}.bind(this);
+					this.tick++;
+				}.bind(this), 250);
+				return this.tick;
+			},
 			
-			this.done(status, responseText, feedback);
-		},
-		uploadFiles: function(files) {
-			
-			var xhr = new XMLHttpRequest();
-
-			var info = {
-				free: function() {
-					
-					xhr.onreadystatechange = null;
-					xhr.upload.onprogress = null;
-					if ( xhr.readyState !== 4 )
-						xhr.abort();
-					this.uploads.splice(this.uploads.indexOf(info), 1);
-				}.bind(this),
-				filenameList: [],
-			}
-			this.uploads.push(info);
-			
-			var prevLoadedBytes = 0;
-			xhr.upload.onprogress = function(ev) {
+			uploaded: function(status, responseText) {
 				
-				if ( !ev.lengthComputable )
-					return;
-				this.loaded += ev.loaded - prevLoadedBytes;
-				prevLoadedBytes = ev.loaded;
-			}.bind(this);
-
-			xhr.onreadystatechange = function() {
+				var feedback = function(success) {
 				
-				if ( xhr.readyState !== 4 )
-					return;
-				info.free();
-				this.uploaded(xhr.status, xhr.responseText);
-			}.bind(this);
-			
-			var fd = new FormData;
-
-			if ( 'data' in this )
-				for ( var name in this.data )
-					fd.append(name, this.data[name]);
-
-			for ( var i = 0; i < files.length; ++i ) {
-				
-				info.filenameList.push(files[i].name);
-				this.total += files[i].size;
-				fd.append('file', files[i]);
-			}
-			
-			xhr.open('POST', this.url, true);
-			xhr.send(fd);
-		},
-		enter: function(ev) {
-			
-			if ( this.uploads.length && !this.multiple ) {
-				
-				this.dropState = 'dropDenied';
-				return;
-			}
-
-			if ( hasFileAPI && hasDataTransferFileSupport(ev.dataTransfer) ) {
-				
-				if ( ('items' in ev.dataTransfer) && ev.dataTransfer.items.length > 1 && !this.multiple )
-					this.dropState = 'dropDenied';
-				else
-					this.dropState = 'dropAllowed';
-			} else {
-				
-				this.dropState = 'dropUndefined';
-			}
-		},
-		leave: function(ev) {
-
-			this.dropState = '';
-		},
-		over: function(ev) {
-
-			if ( hasFileAPI && hasDataTransferFileSupport(ev.dataTransfer) )
-				ev.dataTransfer.dropEffect = (this.dropState === 'dropDenied' ? 'none' : '');
-		},
-		
-		drop: function(ev) {
-			
-			window.setTimeout(function() {
-			
-				this.dropState = '';
-			}.bind(this), 500);
-
-			if ( this.uploads.length && !this.multiple ) {
-				
-				this.dropState = 'dropDenied';
-				return;
-			}
-
-			if ( hasFileAPI && hasDataTransferFileSupport(ev.dataTransfer) ) {
-				
-				var files = Array.prototype.slice.call(ev.dataTransfer.files);
-				var acceptFiles = !files.some(function(file) {
-					
-					return !this.accept(file.name);
-				}.bind(this));
-				
-				if ( !acceptFiles || ev.dataTransfer.files.length === 0 || ev.dataTransfer.files.length > 1 && !this.multiple ) {
-				
-					this.dropState = 'dropDenied';
-				} else {
-					
-					this.uploadFiles(ev.dataTransfer.files);
-				}
-			} else {
-			
-				this.$nextTick(function() {
-
-					if ( ev.target.htmlFor )
-						window.document.getElementById(ev.target.htmlFor).click();
-				});
-			}
-		},
-		onchange: function(ev) {
-			
-			var formElt = ev.target.form;
-			if ( hasFileAPI && 'files' in ev.target ) {
-				
-				this.uploadFiles(ev.target.files);
-				formElt.reset();
-			} else {
-				
-				this.total += NaN;
-				var name = this._uid+'ifr'+Date.now();
-
-				var filename = ev.target.value.match(/[^\/\\]*$/)[0];
-				if ( !this.accept(filename) ) {
-					
-					formElt.reset();
-					this.dropState = 'dropDenied';
-					
+					this.uploadState = success ? 'uploadSuccess' : 'uploadFailure';
 					window.setTimeout(function() {
 					
-						this.dropState = '';
-					}.bind(this), 500);
-					
-					return;
-				}
+						this.uploadState = '';
+					}.bind(this), 1000);
+				}.bind(this);
 				
-				this.uploads.unshift({
-					ifr: name,
+				this.done(status, responseText, feedback);
+			},
+			uploadFiles: function(files) {
+				
+				var xhr = new XMLHttpRequest();
+
+				var info = {
 					free: function() {
 						
-						window.document.getElementsByName(name)[0].src = "about:blank";
-
-						for ( var i = 0; i < this.uploads.length; ++i )
-							if ( this.uploads[i].ifr === name )
-								this.uploads.splice(i, 1);
+						xhr.onreadystatechange = null;
+						xhr.upload.onprogress = null;
+						if ( xhr.readyState !== 4 )
+							xhr.abort();
+						this.uploads.splice(this.uploads.indexOf(info), 1);
 					}.bind(this),
-					filenameList: [filename],
-				});
-
-				this.$nextTick(function() {
+					filenameList: [],
+				}
+				this.uploads.push(info);
+				
+				var prevLoadedBytes = 0;
+				xhr.upload.onprogress = function(ev) {
 					
-					formElt.submit();
+					if ( !ev.lengthComputable )
+						return;
+					this.loaded += ev.loaded - prevLoadedBytes;
+					prevLoadedBytes = ev.loaded;
+				}.bind(this);
+
+				xhr.onreadystatechange = function() {
+					
+					if ( xhr.readyState !== 4 )
+						return;
+					info.free();
+					this.uploaded(xhr.status, xhr.responseText);
+				}.bind(this);
+				
+				var fd = new FormData;
+
+				if ( 'data' in this )
+					for ( var name in this.data )
+						fd.append(name, this.data[name]);
+
+				for ( var i = 0; i < files.length; ++i ) {
+					
+					info.filenameList.push(files[i].name);
+					this.total += files[i].size;
+					fd.append('file', files[i]);
+				}
+				
+				xhr.open('POST', this.url, true);
+				xhr.send(fd);
+			},
+			enter: function(ev) {
+				
+				if ( this.uploads.length && !this.multiple ) {
+					
+					this.dropState = 'dropDenied';
+					return;
+				}
+
+				if ( hasFileAPI && hasDataTransferFileSupport(ev.dataTransfer) ) {
+					
+					if ( ('items' in ev.dataTransfer) && ev.dataTransfer.items.length > 1 && !this.multiple )
+						this.dropState = 'dropDenied';
+					else
+						this.dropState = 'dropAllowed';
+				} else {
+					
+					this.dropState = 'dropUndefined';
+				}
+			},
+			leave: function(ev) {
+
+				this.dropState = '';
+			},
+			over: function(ev) {
+
+				if ( hasFileAPI && hasDataTransferFileSupport(ev.dataTransfer) )
+					ev.dataTransfer.dropEffect = (this.dropState === 'dropDenied' ? 'none' : '');
+			},
+			
+			drop: function(ev) {
+				
+				window.setTimeout(function() {
+				
+					this.dropState = '';
+				}.bind(this), 500);
+
+				if ( this.uploads.length && !this.multiple ) {
+					
+					this.dropState = 'dropDenied';
+					return;
+				}
+
+				if ( hasFileAPI && hasDataTransferFileSupport(ev.dataTransfer) ) {
+					
+					var files = Array.prototype.slice.call(ev.dataTransfer.files);
+					var acceptFiles = !files.some(function(file) {
+						
+						return !this.accept(file.name);
+					}.bind(this));
+					
+					if ( !acceptFiles || ev.dataTransfer.files.length === 0 || ev.dataTransfer.files.length > 1 && !this.multiple ) {
+					
+						this.dropState = 'dropDenied';
+					} else {
+						
+						this.uploadFiles(ev.dataTransfer.files);
+					}
+				} else {
+				
 					this.$nextTick(function() {
-					
-						formElt.reset();
-					});
-				});
-			}
-		},
-		
-		onload: function(iframeElt, name) {
-			
-			var iframeWin = isIFrameWindow(iframeElt);
-			if ( iframeWin !== null && iframeWin.document.location.href === 'about:blank' )
-				return;
-			
-			for ( var i = 0; i < this.uploads.length; ++i )
-				if ( this.uploads[i].ifr === name )
-					this.uploads[i].free();
 
-			this.uploaded(undefined, iframeWin !== null ? iframeWin.document.documentElement.innerText : '');
-		},
-		
-		free: function() {
+						if ( ev.target.htmlFor )
+							window.document.getElementById(ev.target.htmlFor).click();
+					});
+				}
+			},
+			onchange: function(ev) {
+				
+				var formElt = ev.target.form;
+				if ( hasFileAPI && 'files' in ev.target ) {
+					
+					this.uploadFiles(ev.target.files);
+					formElt.reset();
+				} else {
+					
+					this.total += NaN;
+					var name = this._uid+'ifr'+Date.now();
+
+					var filename = ev.target.value.match(/[^\/\\]*$/)[0];
+					if ( !this.accept(filename) ) {
+						
+						formElt.reset();
+						this.dropState = 'dropDenied';
+						
+						window.setTimeout(function() {
+						
+							this.dropState = '';
+						}.bind(this), 500);
+						
+						return;
+					}
+					
+					this.uploads.unshift({
+						ifr: name,
+						free: function() {
+							
+							window.document.getElementsByName(name)[0].src = "about:blank";
+
+							for ( var i = 0; i < this.uploads.length; ++i )
+								if ( this.uploads[i].ifr === name )
+									this.uploads.splice(i, 1);
+						}.bind(this),
+						filenameList: [filename],
+					});
+
+					this.$nextTick(function() {
+						
+						formElt.submit();
+						this.$nextTick(function() {
+						
+							formElt.reset();
+						});
+					});
+				}
+			},
 			
-			for ( var i = this.uploads.length-1; i >= 0 ; --i )
-				this.uploads[i].free();
+			onload: function(iframeElt, name) {
+				
+				var iframeWin = isIFrameWindow(iframeElt);
+				if ( iframeWin !== null && iframeWin.document.location.href === 'about:blank' )
+					return;
+				
+				for ( var i = 0; i < this.uploads.length; ++i )
+					if ( this.uploads[i].ifr === name )
+						this.uploads[i].free();
+
+				this.uploaded(undefined, iframeWin !== null ? iframeWin.document.documentElement.innerText : '');
+			},
+			
+			free: function() {
+				
+				for ( var i = this.uploads.length-1; i >= 0 ; --i )
+					this.uploads[i].free();
+			}
 		}
+	}
+} else {
+
+	module.exports = {
 	}
 }
 
